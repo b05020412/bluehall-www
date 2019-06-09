c1390efc2e72acb88b0965468566b8f8158e5dcc
Hee Won Kim <saillinux@gmail.com>
2019-06-10 03:07:58 +0900

    Update web.tf

--- dev/web.tf
+++ dev/web.tf
@@ -32,6 +32,24 @@ data "aws_subnet" "dev" {
   id    = "${data.aws_subnet_ids.dev.ids[count.index]}"
 }
 
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "1.21.0"
+
+  name                        = "${var.name}"
+  instance_count              = "${var.instance_count}"
+  instance_type               = "${var.instance_type}"
+  ami                         = "${lookup(var.www_ami, var.region)}"
+  key_name                    = "${var.key_name}"
+  vpc_security_group_ids      = ["${data.aws_security_group.dev.id}"]
+  subnet_id                   = "${${data.aws_subnet.dev[0].id}"
+  associate_public_ip_address = true
+  tags = {
+    Owner = "${var.owner}"
+    TTL = "${timestamp()}"
+  }
+}
+
 output "vpc-id" {
   value = "${data.aws_vpc.dev.id}"
 }
