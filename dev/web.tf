63d116a609d4df3684dabffb34420661c4125345
Hee Won Kim <saillinux@gmail.com>
2019-06-10 02:24:06 +0900

    Update web.tf

--- dev/web.tf
+++ dev/web.tf
@@ -2,33 +2,16 @@ provider "aws" {
   region = "${var.region}"
 }
 
-data "terraform_remote_state" "core" {
-  backend = "remote"
-  config = {
-    hostname     = "${var.tfe_hostname}"
-    organization = "${var.organization}"
-    token        = "${var.token}"
-
-    workspaces = {
-      name = "${var.workspace-core}"
-    }
+data "aws_vpc" "dev" {
+  tags {
+    Name = "dev"
   }
 }
 
-module "ec2-instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "1.21.0"
+data "aws_security_group" "default" {
+  vpc_id = "${data.aws_vpc.id}"
+}
 
-  name                        = "${var.name}"
-  instance_count              = "${var.instance_count}"
-  instance_type               = "${var.instance_type}"
-  ami                         = "${lookup(var.www_ami, var.region)}"
-  key_name                    = "${var.key_name}"
-  vpc_security_group_ids      = ["${data.terraform_remote_state.core.default_security_group_id}"]
-  subnet_id                   = "${data.terraform_remote_state.core.public_subnet_ids[0]}"
-  associate_public_ip_address = true
-  tags = {
-    Owner = "${var.owner}"
-    TTL = "${timestamp()}"
-  }
+output "vpc-id" {
+  value = "${data.aws_vpc.id}"
 }
