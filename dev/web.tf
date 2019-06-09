5a82ad193f2e5e2178326e725b7da291df3cda67
Hee Won Kim <saillinux@gmail.com>
2019-06-10 03:45:27 +0900

    Update web.tf

--- dev/web.tf
+++ dev/web.tf
@@ -18,18 +18,8 @@ data "aws_security_groups" "dev" {
   }
 }
 
-data "aws_security_group" "dev" {
-  count = "${length(data.aws_security_groups.dev.ids)}"
-  id    = "${data.aws_security_groups.dev.ids[count.index]}"
-}
-
 data "aws_subnet_ids" "dev" {
   vpc_id = "${var.vpc_id}"
-}
-
-data "aws_subnet" "dev" {
-  count = "${length(data.aws_subnet_ids.dev.ids)}"
-  id    = "${data.aws_subnet_ids.dev.ids[count.index]}"
 }
 
 module "ec2-instance" {
@@ -48,16 +38,4 @@ module "ec2-instance" {
     Owner = "${var.owner}"
     TTL = "${timestamp()}"
   }
-}
-
-output "vpc-id" {
-  value = "${data.aws_vpc.dev.id}"
-}
-
-output "sg-1" {
-  value = ["${data.aws_security_group.dev.*.id}"]
-}
-
-output "subnet_cidr_blocks" {
-  value = ["${data.aws_subnet.dev.*.cidr_block}"]
 }
